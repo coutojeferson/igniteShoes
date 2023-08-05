4c7f54bfc288c62cc66b9f926919469655ad05d8
jeferson.c.silva.glo <jeferson.c.silva@globalhitss.com.br>
2023-08-05 08:49:13 -0300

    Realizado comando expo pre build, para utilizar código nativo.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
 
@@ -8,6 +13,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('1af43d4c-ce92-4c1a-8fa9-0fd14e83356c');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
